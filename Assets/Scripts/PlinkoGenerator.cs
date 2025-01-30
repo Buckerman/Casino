cf2757ea5df07dde55dca08d2ea72d2bfa87317e
DucAnh8530 <s25395@pjwstk.edu.pl>
2025-01-30 20:04:48 +0100

    Plinko Generator #1

---
+++ Assets/Scripts/PlinkoGenerator.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+
+public class PlinkoGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject pegPrefab;
+    [SerializeField] private TMP_Dropdown dropDownRows;
+    [SerializeField] private RectTransform plinkoArea;
+    private float verticalSpacing = 3f;
+    private int numRows = 8;
+
+    void Start()
+    {
+        GeneratePlinkoPyramid();
+    }
+
+    public void GeneratePlinkoPyramid()
+    {
+        // Clear previous pegs
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        numRows = int.Parse(dropDownRows.options[dropDownRows.value].text);
+
+        float areaWidth = plinkoArea.rect.width;
+        float areaHeight = plinkoArea.rect.height;
+
+        float topY = areaHeight / 2;
+
+        int maxPegsInRow = numRows + 2;
+
+        float horizontalSpacing = areaWidth / (maxPegsInRow - 1);
+
+        float verticalSpacing = areaHeight / (numRows + 1);
+
+        for (int row = 0; row < numRows; row++)
+        {
+            int numPegs = row + 3;
+            float rowWidth = (numPegs - 1) * horizontalSpacing;
+            float rowStartX = -rowWidth * 0.5f;
+
+            float rowStartY = topY - (row * verticalSpacing);
+
+            for (int col = 0; col < numPegs; col++)
+            {
+                Vector2 pegPosition = new Vector2(
+                    rowStartX + col * horizontalSpacing,
+                    rowStartY
+                );
+
+                GameObject peg = Instantiate(pegPrefab, transform);
+
+                // Convert local position to world space
+                Vector3 worldPos = plinkoArea.TransformPoint(pegPosition);
+                worldPos.z = 0f; // Ensure pegs are in front of the background image
+                peg.transform.position = worldPos;
+            }
+        }
+    }
+
+}
